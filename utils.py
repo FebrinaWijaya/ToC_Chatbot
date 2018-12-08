0f9c74494e792efb1533dc98548ff31a4f18d620
FebrinaWijaya <febrinawijaya98@gmail.com>
2018-12-08 14:26:01 +0800

    Send text and buttons

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from requests_toolbelt import MultipartEncoder
  
 
 GRAPH_URL = "https://graph.facebook.com/v2.6"
@@ -20,10 +21,39 @@ def send_text_message(id, text):
     return response
 
 
-"""
+
 def send_image_url(id, img_url):
-    pass
+    fb_url = GRAPH_URL + '/me/messages'
+    data = {
+        'recipient': '{"id":'+ id + '}',
+        'message': '{"attachment":{"type":"image", "payload":{}}}'
+    }
+    files = {
+        'filedata': (os.path.basename(img_url), open(img_url, 'rb'), 'image/png')}
+    params = {'access_token': ACCESS_TOKEN}
+    response = requests.post(fb_url, params=params, data=data, files=files)
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
+
+
 
 def send_button_message(id, text, buttons):
-    pass
-"""
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    payload = {
+        "recipient": {"id": id},
+        "message": {"attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"button",
+            "text": text,
+            "buttons": buttons
+          }
+        }}
+    }
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
+        
